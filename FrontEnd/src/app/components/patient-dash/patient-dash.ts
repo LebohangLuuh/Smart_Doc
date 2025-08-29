84a8fb4cda0a18f42d65987e4a1c151dc8ebe983
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-08-30 01:33:16 +0200

    patient dash

---
+++ FrontEnd/src/app/components/patient-dash/patient-dash.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-dash',
+  imports: [],
+  templateUrl: './patient-dash.html',
+  styles: ``
+})
+export class PatientDash {
+
+}
