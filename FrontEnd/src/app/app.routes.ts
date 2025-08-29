b0c8a957d2bedd7674f0ba3973e3d4087931842b
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-08-30 01:29:33 +0200

    created doctor dash ui

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { LandingPage } from './features/landing-page/landing-page';
+import { DoctorDash } from './components/doctor-dash/doctor-dash';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'register', component: Register },
   { path: 'login', component: Login },
   { path: 'home', component: LandingPage},
+  { path: 'doctorDash', component: DoctorDash},
   { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
